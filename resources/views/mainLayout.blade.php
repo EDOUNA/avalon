edd2539b38757b77b6d9aabc6a8ac7f49ccc9ad9
Erwin <erwin.douna@outlook.com>
2019-01-04 21:37:48 +0100

    Fixing main UI

--- resources/views/mainLayout.blade.php
+++ resources/views/mainLayout.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="robots" content="noindex, nofollow, noarchive, noodp, NoImageIndex, noydir">
     <title>{{ env('APP_NAME') }}</title>
+    <meta name="mobile-web-app-capable" content="yes">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <link href="{{ asset('lib/bs/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
